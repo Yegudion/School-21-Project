84ea2605558c30b04338c0187f35e1d7ef798228
Egor <112728493+Yegudion@users.noreply.github.com>
2023-12-11 19:57:16 +0300

    FIX DEFINE

--- s21_matrix/s21_matrix.c
+++ s21_matrix/s21_matrix.c
@@ -55,10 +55,10 @@ int s21_eq_matrix(matrix_t *A, matrix_t *B) {
 }
 
 int s21_sum_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
-  int error = 0;
+  int error = OK;
 
   if (!(A->rows == B->rows && A->columns == B->columns)) {
-    error = 2;
+    error = INCORRECT_MATRIX ;
   } else {
     s21_create_matrix(A->rows, A->columns, result);
     for (int i = 0; i < A->rows; i++) {
